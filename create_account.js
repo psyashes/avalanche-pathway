7bdfd0efd93733bcf2c027c92bbfb18dfdb0de3c
psyashes <43512814+psyashes@users.noreply.github.com>
2021-03-23 00:24:34 +0900

    Check existing key

--- create_account.js
+++ create_account.js
@@ -12,7 +12,24 @@ async function main() {
   const keyChain = chain.keyChain()
   const keyPath = `${credentialsPath}/keypair.json`
 
-  // 2. Generate private key
+  // Check if we already have an existing key
+  if (!fs.existsSync(keyPath)) {
+    console.log("Generating a new keypair...")
+    const key = keyChain.makeKey()
+
+    console.log("Saving keypair to", keyPath)
+    fs.writeFileSync(keyPath, JSON.stringify({
+      pubkey: key.getPublicKeyString(),
+      privkey: key.getPrivateKeyString(),
+    }, null, 2))
+  }
+
+  console.log("Loading credentials into keychain...")
+  const data = JSON.parse(fs.readFileSync(keyPath))
+
+  const key = keyChain.importKey(data.privkey)
+  console.log("Imported X-chain address:", key.getAddressString())
+
   // 3. Check address balance
 }
 
