355e8907343419041d85c7e94e06dc7d393e37ec
psyashes <43512814+psyashes@users.noreply.github.com>
2021-03-24 00:11:43 +0900

    Prepare transaction

--- transfer.js
+++ transfer.js
@@ -16,7 +16,35 @@ async function main() {
   const data = JSON.parse(fs.readFileSync(`${credentialsPath}/keypair.json`))
   const key = keychain.importKey(data.privkey)
 
-  // 2. Prepare transaction
+  // Fetch UTXO (i.e unspent transaction outputs)
+  const address = key.getAddressString()
+  const { utxos } = await chain.getUTXOs(address)
+
+  // Prepare transaction details
+  const receiver = "X-fuji1j2zasjlkkvptegp6dpm222q6sn02k0rp9fj92d" // Pathway test receiver address
+  const amount = "50000000" // Total amount we're transferring = 0.05 AVAX
+  const asset = "AVAX" // Primary asset used for the transaction (Avalanche supports many)
+
+  // Determine the real asset ID from its symbol/alias
+  // We can also get the primary asset ID with chain.getAVAXAssetID() call
+  const assetInfo = await chain.getAssetDescription(asset)
+  const assetID = binTools.cb58Encode(assetInfo.assetID)
+
+  // Fetch our current balance
+  let balance = await chain.getBalance(address, assetID)
+  console.log("Balance before sending tx:", balance)
+
+  // Generate a new transaction
+  const unsignedTx = await chain.buildBaseTx(
+    utxos, // unspent outputs
+    new avalanche.BN(amount), // transaction amount formatted as a BigNumber
+    assetID, // AVAX asset
+    [receiver], // addresses to send the funds
+    [address], // addresses being used to send the funds from the UTXOs provided
+    [address], // addresses that can spend the change remaining from the spent UTXOs
+    binTools.stringToBuffer("Figment Pathway") // memo, totally optional
+  )
+
   // 3. Send transaction to network
 }
 
